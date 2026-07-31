45d55376f64610aca2cb33e37a7639b1bc7698ea
edersoncm14-ai <ederson.cm14@gmail.com>
2026-07-31 13:32:24 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,15 @@
-export const auth={}; export const db={};
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+ 
+const firebaseConfig = {
+apiKey: "SUA_API_KEY",
+authDomain: "SEU_PROJETO.firebaseapp.com",
+projectId: "SEU_PROJETO",
+storageBucket: "SEU_PROJETO.appspot.com",
+messagingSenderId: "XXXXXXXX",
+appId: "XXXXXXXX"
+};
+ 
+const app = initializeApp(firebaseConfig);
+ 
+export const db = getFirestore(app);
