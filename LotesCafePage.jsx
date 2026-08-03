a0101c26de00b18d074b47855f3e629013113412
edersoncm14-ai <ederson.cm14@gmail.com>
2026-08-03 11:54:59 -0300

    Update LotesCafePage.jsx

--- LotesCafePage.jsx
+++ LotesCafePage.jsx
@@ -1,26 +1,54 @@
-xport default function LotesCafePage() {
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./firebase";
+ 
+export default function LotesCafePage() {
+const [lotes, setLotes] = useState([]);
+ 
+useEffect(() => {
+async function carregarLotes() {
+const querySnapshot = await getDocs(
+collection(db, "lotes_cafe")
+);
+ 
+const dados = querySnapshot.docs.map(doc => ({
+id: doc.id,
+...doc.data()
+}));
+ 
+setLotes(dados);
+}
+ 
+carregarLotes();
+}, []);
+ 
 return (
 <div style={{ padding: "20px" }}>
 <h1>☕ Lotes de Café</h1>
  
-<p>Cadastro e gerenciamento dos lotes.</p>
- 
-<div
+<table
  style={{
+ width: "100%",
  background: "#fff",
- padding: "20px",
- borderRadius: "10px",
- marginTop: "20px"
+ borderCollapse: "collapse"
  }}
  >
-<h3>Exemplo</h3>
+<thead>
+<tr>
+<th>Código</th>
+<th>Propriedade</th>
+</tr>
+</thead>
  
-<p>Código: SC2026-001</p>
-<p>Propriedade: Sítio Santa Clara</p>
-<p>Bebida: Mole</p>
-<p>Sacas: 120</p>
-</div>
+<tbody>
+{lotes.map((lote) => (
+<tr key={lote.id}>
+<td>{lote.codigo}</td>
+<td>{lote.propriedade}</td>
+</tr>
+))}
+</tbody>
+</table>
 </div>
 );
 }
-``
