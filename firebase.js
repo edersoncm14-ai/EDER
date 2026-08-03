439d2d17aa82e12ea5050ca66a25a27b560157ff
edersoncm14-ai <ederson.cm14@gmail.com>
2026-08-03 11:16:22 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,16 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+ 
 const firebaseConfig = {
-  apiKey: "AIzaSyAugfSmX4tNmChxGPE3hMe2QDrECDFFvrk",
-  authDomain: "cafeerp-32b7a.firebaseapp.com",
-  projectId: "cafeerp-32b7a",
-  storageBucket: "cafeerp-32b7a.firebasestorage.app",
-  messagingSenderId: "616671677234",
-  appId: "1:616671677234:web:944164759933e5004f252b",
-  measurementId: "G-S9NV4JFE0L"
+apiKey: "AIzaSyAugfSmX4tNmChxGPE3HMe2QDrECDFFvrk",
+authDomain: "cafeerp-32b7a.firebaseapp.com",
+projectId: "cafeerp-32b7a",
+storageBucket: "cafeerp-32b7a.firebasestorage.app",
+messagingSenderId: "616671677234",
+appId: "1:616671677234:web:944164759933e5004f252b",
+measurementId: "G-S9NV4JFE0L"
 };
+ 
+const app = initializeApp(firebaseConfig);
+ 
+export const db = getFirestore(app);
