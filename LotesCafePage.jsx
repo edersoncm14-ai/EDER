dae7a16ca72ae35a9d2b4c58c6bf0e4e4a72dc82
edersoncm14-ai <ederson.cm14@gmail.com>
2026-08-14 22:25:50 -0300

    Update LotesCafePage.jsx

--- LotesCafePage.jsx
+++ LotesCafePage.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
 import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 import { db } from "./firebase";
 
