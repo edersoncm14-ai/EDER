92dfc6085ed9386b349f3cf3bf9896b50a365105
edersoncm14-ai <ederson.cm14@gmail.com>
2026-08-20 16:54:49 -0300

    Obscure sensitive Firebase configuration details
    Updated firebase configuration to hide sensitive information.

--- firebase.js
+++ firebase.js
@@ -1,16 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
- 
+
 const firebaseConfig = {
-apiKey: "AIzaSyAugfSmX4tNmChxGPE3HMe2QDrECDFFvrk",
-authDomain: "cafeerp-32b7a.firebaseapp.com",
-projectId: "cafeerp-32b7a",
-storageBucket: "cafeerp-32b7a.firebasestorage.app",
-messagingSenderId: "616671677234",
-appId: "1:616671677234:web:944164759933e5004f252b",
-measurementId: "G-S9NV4JFE0L"
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "...",
+  measurementId: "..."
 };
- 
+
 const app = initializeApp(firebaseConfig);
- 
+
 export const db = getFirestore(app);
